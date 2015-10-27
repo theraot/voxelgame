bfcb4b48369cbddf8bca93cd1ffd9cd0f5227c1f
Alfonso J. Ramos <theraot@gmail.com>
2015-10-27 09:36:23 -0500

    Using for stream disposal

--- Blox/GameActions/GetWorld.cs
+++ Blox/GameActions/GetWorld.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using Hexpoint.Blox.Hosts.World;
+using System;
 using System.IO;
 using System.IO.Compression;
-using Hexpoint.Blox.Hosts.World;
 
 namespace Hexpoint.Blox.GameActions
 {
 	internal class GetWorld : GameAction
 	{
-		internal override ActionType ActionType { get { return ActionType.GetWorld; } }
+		internal override ActionType ActionType
+		{
+			get
+			{
+				return ActionType.GetWorld;
+			}
+		}
+
 		public int UncompressedLength { get; set; }
+
 		public override string ToString()
 		{
 			return "GetWorld";
@@ -18,42 +26,42 @@ namespace Hexpoint.Blox.GameActions
 		{
 			if (!Facade.Configuration.IsServer) throw new Exception("Only the server needs this");
 			Immediate = true;
-
 			//bm: this does not need to be locked. locking it prevents other actions from being queued, in turn locking other Players' threads and stalling new connections
-			var memstream = new MemoryStream();
-			var gzstream = new DeflateStream(memstream, CompressionMode.Compress, true);
-			//DeflateStream only applies compression during .Write, writing 2 bytes at a time ends up inflating it a lot. Adding this saves up to 99.3%
-			//http://code.logos.com/blog/2012/06/always-wrap-gzipstream-with-bufferedstream.html
-			var buffstream = new BufferedStream(gzstream, 65536);
-
-			if (WorldData.Chunks[0,0] != null)
+			using (var memstream = new MemoryStream())
 			{
-				var worldSettings = WorldSettings.GetXmlByteArray(); //get current world settings to send to the client, includes chunk settings, mobs, clutter, sun properties, etc.
-				buffstream.Write(BitConverter.GetBytes(worldSettings.Length), 0, sizeof(int));
-				UncompressedLength += sizeof(int);
-				buffstream.Write(worldSettings, 0, worldSettings.Length); //write the length of the world config xml
-				UncompressedLength += worldSettings.Length;
-
-				var chunkBytes = new byte[Chunk.SIZE_IN_BYTES];
-				for (var x = 0; x < WorldData.SizeInChunksX; x++)
+				using (var gzstream = new DeflateStream(memstream, CompressionMode.Compress, true))
 				{
-					for (var z = 0; z < WorldData.SizeInChunksZ; z++)
+					//DeflateStream only applies compression during .Write, writing 2 bytes at a time ends up inflating it a lot. Adding this saves up to 99.3%
+					//http://code.logos.com/blog/2012/06/always-wrap-gzipstream-with-bufferedstream.html
+					using (var buffstream = new BufferedStream(gzstream, 65536))
 					{
-						
-						Buffer.BlockCopy(WorldData.Chunks[x, z].Blocks.Array, 0, chunkBytes, 0, chunkBytes.Length);
-						buffstream.Write(chunkBytes, 0, chunkBytes.Length);
-						UncompressedLength += chunkBytes.Length;
+						if (WorldData.Chunks[0, 0] != null)
+						{
+							var worldSettings = WorldSettings.GetXmlByteArray(); //get current world settings to send to the client, includes chunk settings, mobs, clutter, sun properties, etc.
+							buffstream.Write(BitConverter.GetBytes(worldSettings.Length), 0, sizeof(int));
+							UncompressedLength += sizeof(int);
+							buffstream.Write(worldSettings, 0, worldSettings.Length); //write the length of the world config xml
+							UncompressedLength += worldSettings.Length;
+							var chunkBytes = new byte[Chunk.SIZE_IN_BYTES];
+							for (var x = 0; x < WorldData.SizeInChunksX; x++)
+							{
+								for (var z = 0; z < WorldData.SizeInChunksZ; z++)
+								{
+									Buffer.BlockCopy(WorldData.Chunks[x, z].Blocks.Array, 0, chunkBytes, 0, chunkBytes.Length);
+									buffstream.Write(chunkBytes, 0, chunkBytes.Length);
+									UncompressedLength += chunkBytes.Length;
+								}
+							}
+							buffstream.Flush();
+							gzstream.Close(); //the close flushes the gzstream, .Flush doesnt work
+							buffstream.Close();
+							DataLength = (int)memstream.Length;
+						}
+						base.Queue();
+						Write(memstream.ToArray(), (int)memstream.Length); //bm todo: bit of a pig? does it matter?
 					}
 				}
-				buffstream.Flush();
-				gzstream.Close(); //the close flushes the gzstream, .Flush doesnt work
-				buffstream.Close();
-				DataLength = (int)memstream.Length;
 			}
-
-			base.Queue();
-			Write(memstream.ToArray(), (int)memstream.Length); //bm todo: bit of a pig? does it matter?
-			memstream.Close();
 		}
 
 		internal override void Receive()
@@ -71,53 +79,53 @@ namespace Hexpoint.Blox.GameActions
 					bytesRead += TcpClient.GetStream().Read(response, bytesRead, Math.Min(DataLength - bytesRead, 4096));
 					//System.Threading.Thread.Sleep(50); //simulate remote connect
 				} while (bytesRead < DataLength);
-				
-				var memstream = new MemoryStream(response);
-				var gzstream = new DeflateStream(memstream, CompressionMode.Decompress);
 
-				var worldSettingsSizeBytes = new byte[sizeof(int)];
-				bytesRead = 0;
-				while (bytesRead < sizeof(int))
+				using (var memstream = new MemoryStream(response))
 				{
-					bytesRead += gzstream.Read(worldSettingsSizeBytes, bytesRead, sizeof(int) - bytesRead); //read the size of the world config xml
-				}
-				UncompressedLength = bytesRead;
-				var worldSettingsBytes = new byte[BitConverter.ToInt32(worldSettingsSizeBytes, 0)];
-
-				bytesRead = 0;
-				while (bytesRead < worldSettingsBytes.Length)
-				{
-					bytesRead += gzstream.Read(worldSettingsBytes, bytesRead, worldSettingsBytes.Length - bytesRead);
-				}
-				UncompressedLength += bytesRead;
-				WorldSettings.LoadSettings(worldSettingsBytes);
-
-				var chunkTotal = WorldData.SizeInChunksX * WorldData.SizeInChunksZ;
-				var chunkCount = 1;
-				for (var x = 0; x < WorldData.SizeInChunksX; x++) //loop through each chunk and load it
-				{
-					for (var z = 0; z < WorldData.SizeInChunksZ; z++)
+					using (var gzstream = new DeflateStream(memstream, CompressionMode.Decompress))
 					{
-						Settings.Launcher.UpdateProgressInvokable(string.Format("Loading Chunks: {0} / {1}", chunkCount, chunkTotal), chunkCount, chunkTotal);
-						
+						var worldSettingsSizeBytes = new byte[sizeof(int)];
 						bytesRead = 0;
-						var chunkBytes = new byte[Chunk.SIZE_IN_BYTES];
-						while (bytesRead < chunkBytes.Length)
+						while (bytesRead < sizeof(int))
 						{
-							var byteCount = gzstream.Read(chunkBytes, bytesRead, chunkBytes.Length - bytesRead);
-							if (byteCount <= 0) throw new Exception("Received incomplete world."); //gm: was sometimes getting zero here because the gzipstream wasnt getting flushed properly, might as well leave the check here
-							bytesRead += byteCount;
+							bytesRead += gzstream.Read(worldSettingsSizeBytes, bytesRead, sizeof(int) - bytesRead); //read the size of the world config xml
+						}
+						UncompressedLength = bytesRead;
+						var worldSettingsBytes = new byte[BitConverter.ToInt32(worldSettingsSizeBytes, 0)];
+
+						bytesRead = 0;
+						while (bytesRead < worldSettingsBytes.Length)
+						{
+							bytesRead += gzstream.Read(worldSettingsBytes, bytesRead, worldSettingsBytes.Length - bytesRead);
 						}
 						UncompressedLength += bytesRead;
-						
-						WorldData.LoadChunk(WorldData.Chunks[x, z], chunkBytes);
+						WorldSettings.LoadSettings(worldSettingsBytes);
 
-						chunkCount++;
+						var chunkTotal = WorldData.SizeInChunksX * WorldData.SizeInChunksZ;
+						var chunkCount = 1;
+						for (var x = 0; x < WorldData.SizeInChunksX; x++) //loop through each chunk and load it
+						{
+							for (var z = 0; z < WorldData.SizeInChunksZ; z++)
+							{
+								Settings.Launcher.UpdateProgressInvokable(string.Format("Loading Chunks: {0} / {1}", chunkCount, chunkTotal), chunkCount, chunkTotal);
+
+								bytesRead = 0;
+								var chunkBytes = new byte[Chunk.SIZE_IN_BYTES];
+								while (bytesRead < chunkBytes.Length)
+								{
+									var byteCount = gzstream.Read(chunkBytes, bytesRead, chunkBytes.Length - bytesRead);
+									if (byteCount <= 0) throw new Exception("Received incomplete world."); //gm: was sometimes getting zero here because the gzipstream wasnt getting flushed properly, might as well leave the check here
+									bytesRead += byteCount;
+								}
+								UncompressedLength += bytesRead;
+
+								WorldData.LoadChunk(WorldData.Chunks[x, z], chunkBytes);
+
+								chunkCount++;
+							}
+						}
 					}
 				}
-
-				gzstream.Dispose();
-				memstream.Dispose();
 			}
 
 			WorldData.InitializeAllLightMaps();
